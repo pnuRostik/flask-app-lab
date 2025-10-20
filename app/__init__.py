e61c25f1cd84c155818681cb6eacc24fd0e7d495
Rostick Vasaylyk <pomoju.boje@gmail.com>
2025-10-20 16:03:30 +0300

    initial commit user bp

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+from . import views
+
+from .users import users_bp
+#from .products import products_bp
+
+app.register_blueprint(users_bp)
+#app.register_blueprint(products_bp)

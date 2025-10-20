9c7e1a671ff359ff66ee8311dad8a1d975ed952b
Rostick Vasaylyk <pomoju.boje@gmail.com>
2025-10-20 16:16:46 +0300

    bp routing product

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 from . import views
 
 from .users import users_bp
-#from .products import products_bp
+from .products import products_bp
 
 app.register_blueprint(users_bp)
-#app.register_blueprint(products_bp)
+app.register_blueprint(products_bp)
